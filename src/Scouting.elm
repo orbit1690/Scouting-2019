d399934573df7d7af4ddfa29b6fd6c3bb2cb859c
Hadar <nahshon.hadar@gmail.com>
2019-12-03 00:45:07 +0200

    determines driverStation automatically!

--- src/Scouting.elm
+++ src/Scouting.elm
@@ -35,17 +35,29 @@ type alias Model =
     }
 
 
-matchOrganizer : Int -> Int -> Int -> Int -> Int -> Int -> List ( Int, String )
-matchOrganizer t1 t2 t3 t4 t5 t6 =
-    [ ( t1, "כחול 1" ), ( t2, "כחול 2" ), ( t3, "כחול 3" ), ( t4, "אדום 1" ), ( t5, "אדום 2" ), ( t6, "אדום 3" ) ]
+
+{-
+   matchOrganizer : Int -> Int -> Int -> Int -> Int -> Int -> List ( Int, String )
+   matchOrganizer t1 t2 t3 t4 t5 t6 =
+       [ ( t1, "כחול 1" ), ( t2, "כחול 2" ), ( t3, "כחול 3" ), ( t4, "אדום 1" ), ( t5, "אדום 2" ), ( t6, "אדום 3" ) ]
 
 
-driverStations : List (List ( Int, String ))
+   driverStations : List (List ( Int, String ))
+   driverStations =
+       [ matchOrganizer 1690 1574 3339 254 2056 1323
+       , matchOrganizer 118 1577 1024 2056 1690 254
+       , matchOrganizer 1574 3339 1577 1323 1024 118
+       , matchOrganizer 3339 1574 1577 1024 118 2056
+       ]
+-}
+
+
+driverStations : List (List Int)
 driverStations =
-    [ matchOrganizer 1690 1574 3339 254 2056 1323
-    , matchOrganizer 118 1577 1024 2056 1690 254
-    , matchOrganizer 1574 3339 1577 1323 1024 118
-    , matchOrganizer 3339 1574 1577 1024 118 2056
+    [ [ 1690, 1574, 3339, 254, 2056, 1323 ]
+    , [ 118, 1577, 1024, 2056, 1690, 254 ]
+    , [ 1574, 3339, 1577, 1323, 1024, 118 ]
+    , [ 3339, 1574, 1577, 1024, 118, 2056 ]
     ]
 
 
@@ -70,16 +82,19 @@ update msg model =
         TeamInput input ->
             { model
                 | team = String.toInt input
+                , driverStation = indexToName (String.toInt input) model.match
             }
 
         MatchInput input ->
             { model
                 | match = String.toInt input
+                , driverStation = indexToName model.team (String.toInt input)
             }
 
         Start ->
             { model
                 | isStarted = not model.isStarted
+                , driverStation = indexToName model.team model.match
             }
 
 
@@ -88,9 +103,10 @@ unwrapToString maybeInt =
     unwrap "" String.fromInt maybeInt
 
 
-getMatchStations : Int -> List ( Int, String )
+getMatchStations : Int -> List Int
 getMatchStations match =
-    withDefault [ ( 0, "" ) ] <| getAt match driverStations
+    -- == [team1, team2, team3, team4, team5, team6]
+    withDefault [ 0 ] <| getAt match driverStations
 
 
 
@@ -101,15 +117,32 @@ getMatchStations match =
 -}
 
 
-driverStationChooser : Maybe Int -> Maybe Int -> String
-driverStationChooser team match =
-    case elemIndex of
-        a ->
-            if first a == team then
-                second a
+stationIndex : Maybe Int -> Maybe Int -> String
+stationIndex team match =
+    -- team2 -> index = 2
+    unwrapToString << elemIndex (withDefault 0 team) <| getMatchStations (withDefault 0 match)
 
-            else
-                ""
+
+indexToName : Maybe Int -> Maybe Int -> String
+indexToName team match =
+    case stationIndex team match of
+        "1" ->
+            "כחול 1"
+
+        "2" ->
+            "כחול 2"
+
+        "3" ->
+            "כחול 3"
+
+        "4" ->
+            "אדום 1"
+
+        "5" ->
+            "אדום 2"
+
+        "6" ->
+            "אדום 3"
 
         _ ->
             ""
@@ -156,7 +189,7 @@ view model =
 view1 : Model -> Html.Html Msg
 view1 model =
     Html.div []
-        [ Html.text <| "Pre-Scout screen:   " ++ driverStationChooser model.match model.team
+        [ Html.text <| "Pre-Scout screen:   " ++ model.driverStation
         , Html.div []
             [ inputs "Scouter's name:" NameInput model.scouter
             , inputs "Team's number:" TeamInput << unwrapToString <| model.team
@@ -181,6 +214,10 @@ view2 model =
                 , "\nstation - "
                 , model.driverStation
                 , "\n\n"
+                , "match1 = [ 1690, 1574, 3339 || 254, 2056, 1323 ]\n"
+                , "match2 = [ 118,  1577, 1024 || 2056, 1690, 254 ]\n"
+                , "match3 = [ 1574, 3339, 1577 || 1323, 1024, 118 ]\n"
+                , "match4 = [ 3339, 1574, 1577 || 1024, 118, 2056 ]"
                 ]
         , startButton model
         ]
