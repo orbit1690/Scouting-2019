f473cb4dd6adffa699e856578f0a88b3fd94cbbc
Yehonatan Cohen <yoncohenyon@gmail.com>
2019-12-09 20:16:57 +0200

    started second issue

--- src/Scouting.elm
+++ src/Scouting.elm
@@ -1,65 +1,115 @@
-module Scouting exposing (Msg, Model, init, update, main, view )
+module Scouting exposing (Model, Msg, init, main, update, view)
 
 import Browser
+import Html exposing (input, text)
 import Html.Attributes exposing (placeholder, value)
-import Html exposing (input , text)
-import Html.Events exposing (onClick , onInput)
+import Html.Events exposing (onClick, onInput)
+import TeamsDriverStationDict
 
 
-inputwriter : String -> (String -> Msg) -> String -> Html.Html Msg
-inputwriter placeholderInput msg model =
-    Html.div [] [
-    input [placeholder placeholderInput, onInput msg, value model] []
-    ,text "\n"  
-    ]
+inputWriter : String -> (String -> Msg) -> String -> Html.Html Msg
+inputWriter placeholderInput msg model =
+    Html.div [ Html.Attributes.style "margin-bottom" "1%" ]
+        [ input [ placeholder placeholderInput, onInput msg, value model ] []
+        ]
 
-type Msg =
-    ScoutersNameInput String
+
+type Pressed
+    = Pressed
+    | NotPressed
+    | InputError
+
+
+inputChecker : Model -> Model
+inputChecker model =
+    if model.scoutersName == "" || model.scoutedTeamNumber == "" || model.scoutedTeamDriverStationPosition == "" || model.matchNumber == "" then
+        { model | isPressed = InputError }
+
+    else
+        { model | isPressed = Pressed }
+
+
+type Msg
+    = ScoutersNameInput String
     | TeamNumberInput String
     | MatchNumberInput String
     | DriverStationInput String
-    | Page Bool
+    | Page Pressed
 
 
 type alias Model =
-    {scoutersName : String
+    { scoutersName : String
     , scoutedTeamNumber : String
     , matchNumber : String
     , scoutedTeamDriverStationPosition : String
-    , isPage : Bool
+    , isPressed : Pressed
     }
 
+
 main : Program () Model Msg
-main = 
-    Browser.element{init = always (init, Cmd.none), update = update, view = view, subscriptions = always Sub.none }
+main =
+    Browser.element
+        { init = always ( init, Cmd.none )
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
+
 
 init : Model
-init = {scoutersName = "", scoutedTeamNumber = "", matchNumber = "", scoutedTeamDriverStationPosition = "", isPage = False}
+init =
+    { scoutersName = "", scoutedTeamNumber = "", matchNumber = "", scoutedTeamDriverStationPosition = "", isPressed = NotPressed }
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = 
-    case msg of 
-        ScoutersNameInput string ->
-            ({model | scoutersName = string }, Cmd.none)
-        TeamNumberInput string ->
-            ({model | scoutedTeamNumber = string }, Cmd.none)
-        MatchNumberInput string ->
-            ({model | matchNumber = string }, Cmd.none)
-        DriverStationInput string ->
-            ({model | scoutedTeamDriverStationPosition = string }, Cmd.none)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ScoutersNameInput name ->
+            ( { model | scoutersName = name }, Cmd.none )
+
+        TeamNumberInput name ->
+            ( { model | scoutedTeamNumber = name }, Cmd.none )
+
+        MatchNumberInput name ->
+            ( { model | matchNumber = name }, Cmd.none )
+
+        DriverStationInput name ->
+            ( { model | scoutedTeamDriverStationPosition = name }, Cmd.none )
+
         Page state ->
-            ({model | isPage = state}, Cmd.none)
+            ( inputChecker model, Cmd.none )
 
-view : Model ->  Html.Html Msg
-view model = 
-    if model.isPage == True then
-        Html.text model.scoutersName
-    else 
 
-    Html.pre [][ 
-        inputwriter "your name" ScoutersNameInput model.scoutersName
-        ,inputwriter "scouted team number" TeamNumberInput model.scoutedTeamNumber
-        ,inputwriter "match number" MatchNumberInput model.matchNumber
-        ,inputwriter  "driver station position" DriverStationInput model.scoutedTeamDriverStationPosition
-        , Html.button [onClick <| Page True] [Html.text "second page"] 
+viewWhenPressed : Model -> Html.Html Msg
+viewWhenPressed model =
+    Html.pre []
+        [ inputWriter "your name" ScoutersNameInput model.scoutersName
+        , inputWriter "scouted team number" TeamNumberInput model.scoutedTeamNumber
+        , inputWriter "match number" MatchNumberInput model.matchNumber
+        , inputWriter "driver station position" DriverStationInput model.scoutedTeamDriverStationPosition
+        , Html.button [ onClick <| Page Pressed ] [ Html.text "second page" ]
         ]
+
+
+view : Model -> Html.Html Msg
+view model =
+    if model.isPressed == Pressed then
+        Html.pre []
+            [ Html.text model.scoutersName
+            , Html.text " your name\n"
+            , Html.text model.scoutedTeamNumber
+            , Html.text " scouted team number\n"
+            , Html.text model.matchNumber
+            , Html.text " match number\n"
+            , Html.text model.scoutedTeamDriverStationPosition
+            , Html.text " driver station position\n"
+            ]
+
+    else if model.isPressed == InputError then
+        Html.div []
+            [ viewWhenPressed model
+            , Html.text "all fields are required"
+            ]
+
+    else
+        viewWhenPressed model
---
+++ src/TeamsDriverStationDict.elm
@@ -0,0 +1,11 @@
+module TeamsDriverStationDict exposing (..)
+
+
+driverStationInfo : Model -> 
+driverStationInfo = {matchNumber = {1 = {blue1 = 1, blue2 = 2, blue3 = 3, red1 = 4, red2 = 5, red3 = 6}
+                                    ,2 = {blue1 = 7, blue2 = 8, blue3 = 9, red1 = 10, red2 = 11, red3 = 12}
+                                    ,3 = {blue1 = 13, blue2 = 14, blue3 = 15, red1 = 16, red2 = 17, red3 = 18}
+                    }
+
+{matchNumber : {stations : {blueone : Int, bluetwo : Int, bluethree : Int, redone : Int, redtwo : Int, redthree : Int} }
+}
