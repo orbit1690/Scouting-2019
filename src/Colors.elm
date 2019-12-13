9ccff0f051dfcdda550a9b8bbb7e79ae8972edd0
Itamar shaked <itamar.shaked10@gmail.com>
2019-12-13 23:07:29 +0200

    Colors library for everyone

---
+++ src/Colors.elm
@@ -0,0 +1,59 @@
+module Colors exposing (black, blue, blueGreen, grass, gray, green, lightBlue, orange, pink, purple, red, sky, white, yellow)
+
+import Element
+
+
+red =
+    Element.rgb255 255 0 0
+
+
+orange =
+    Element.rgb255 255 153 51
+
+
+yellow =
+    Element.rgb255 255 255 0
+
+
+grass =
+    Element.rgb255 127 255 0
+
+
+green =
+    Element.rgb255 0 255 0
+
+
+blueGreen =
+    Element.rgb255 0 255 128
+
+
+lightBlue =
+    Element.rgb255 0 255 255
+
+
+sky =
+    Element.rgb255 0 127 255
+
+
+blue =
+    Element.rgb255 0 0 255
+
+
+purple =
+    Element.rgb255 127 0 255
+
+
+pink =
+    Element.rgb255 255 107 178
+
+
+white =
+    Element.rgb255 255 255 255
+
+
+black =
+    Element.rgb255 0 0 0
+
+
+gray =
+    Element.rgb255 128 128 128
